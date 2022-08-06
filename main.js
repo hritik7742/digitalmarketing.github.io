90c8ffad299b2bde79a36e38ad60ed5ee219a20e
hritik <hritikchoudharykota@gmail.com>
2022-08-06 17:06:00 +0530

    market

---
+++ main.js
@@ -0,0 +1,18 @@
+// var ullist=document.getElementById("oltag") 
+// var secton=document.getElementById('homepage') 
+// function option(){
+// ullist.style.left="0";
+// secton.style.display="none"
+
+   
+// }
+
+// function remove(){
+//     ullist.style.left="-84pc"
+//     secton.style.display="block"
+//     secton.style.transition="0s"
+// }
+// function changecolor(){
+//     document.body.style.backgroundColor="black"
+//     document.getElementById("secondnav").style.backgroundColor="black"
+// }
